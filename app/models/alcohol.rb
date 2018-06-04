4dee7330b55ad2f5af328bbbe8bc700c6ed4fcb8
Raghu Betina <raghu@firstdraft.com>
2018-06-04 18:52:42 +0000

    Implement image uploads

--- app/models/alcohol.rb
+++ app/models/alcohol.rb
@@ -1,4 +1,6 @@
 class Alcohol < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   # Direct associations
 
   has_many   :pairings,
--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -1,4 +1,6 @@
 class Cigar < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   # Direct associations
 
   has_many   :pairings,
